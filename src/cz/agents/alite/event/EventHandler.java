da165a160650684aa256df1b43525a2bf224b557
Antonin Komenda <997305+gree7@users.noreply.github.com>
2010-04-27 18:16:47 +0200

    Changed API naming

--- src/cz/agents/alite/event/EventHandler.java
+++ src/cz/agents/alite/event/EventHandler.java
@@ -3,7 +3,7 @@ package cz.agents.alite.event;
 
 public interface EventHandler {
 
-    public EventProcessor getSimulation();
+    public EventProcessor getEventProcessor();
 
     public void handleEvent(Event event);
 
