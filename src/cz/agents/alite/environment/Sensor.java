eabe3b8d93bdfc3620ec3ff61323ecdc7d4884c5
gree <gree@grees.net>
2010-05-05 15:31:55 +0200

    Pulled up environment, sensor, and action general methods

--- src/cz/agents/alite/environment/Sensor.java
+++ src/cz/agents/alite/environment/Sensor.java
@@ -1,5 +1,23 @@
 package cz.agents.alite.environment;
 
+import cz.agents.alite.entity.Entity;
+
 public class Sensor {
+
+    private final Environment environment;
+    private final Entity relatedEntity;
+
+    public Sensor(Environment environment, Entity relatedEntity) {
+        this.environment = environment;
+        this.relatedEntity = relatedEntity;
+    }
+
+    protected Environment getEnvironment() {
+        return environment;
+    }
+
+    protected Entity getRelatedEntity() {
+        return relatedEntity;
+    }
 
 }
