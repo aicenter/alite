9311155864d79abd5aff6ec74cdbc931a98c7c6f
Antonin Komenda <997305+gree7@users.noreply.github.com>
2010-05-05 15:31:55 +0200

    Pulled up environment, sensor, and action general methods

--- src/cz/agents/alite/environment/Action.java
+++ src/cz/agents/alite/environment/Action.java
@@ -1,5 +1,23 @@
 package cz.agents.alite.environment;
 
+import cz.agents.alite.entity.Entity;
+
 public class Action {
+
+    private final Environment environment;
+    private final Entity relatedEntity;
+
+    public Action(Environment environment, Entity relatedEntity) {
+        this.environment = environment;
+        this.relatedEntity = relatedEntity;
+    }
+
+    protected Environment getEnvironment() {
+        return environment;
+    }
+
+    protected Entity getRelatedEntity() {
+        return relatedEntity;
+    }
 
 }
--- src/cz/agents/alite/environment/Environment.java
+++ src/cz/agents/alite/environment/Environment.java
@@ -1,5 +1,58 @@
 package cz.agents.alite.environment;
 
+import java.util.Random;
+
+import cz.agents.alite.cross.environment.eventbased.EventBasedAction;
+import cz.agents.alite.cross.environment.eventbased.EventBasedSensor;
+import cz.agents.alite.entity.Entity;
+
 public class Environment {
+
+    private final Handler handler;
+    private Random random = new Random();
+
+    public Environment() {
+        handler = new Handler();
+    }
+
+    public Environment.Handler handler() {
+        return handler;
+    }
+
+    public class Handler {
+
+        protected Handler() {
+        }
+
+        public <C extends EventBasedAction> C addAction(Class<C> clazz, Entity entity) {
+            C instance = null;
+
+            try {
+                instance = clazz.getConstructor(Environment.class, Entity.class)
+                        .newInstance(Environment.this, entity);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return instance;
+        }
+
+        public <C extends EventBasedSensor> C addSensor(Class<C> clazz, Entity entity) {
+            C instance = null;
+
+            try {
+                instance = clazz.getConstructor(Environment.class, Entity.class)
+                        .newInstance(Environment.this, entity);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return instance;
+        }
+
+        public Random getRandom() {
+            return random;
+        }
+    }
 
 }
