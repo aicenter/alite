2a246fefc360bb448acaa4fab489f0f7d555eb7d
milenovsky <milenovsky@localhost>
2010-07-10 11:16:32 +0000

    Prepaired for GE showing agents in middle of paths

--- src/cz/agents/alite/googleearth/handler/GoogleEarthHandler.java
+++ src/cz/agents/alite/googleearth/handler/GoogleEarthHandler.java
@@ -10,47 +10,60 @@ import com.sun.net.httpserver.HttpHandler;
 import cz.agents.alite.googleearth.KmlFileCreator;
 
 /**
- * Abstract class for Google Earth requests. Extend this class to create
- * another handler for separate GE layer.
- *
+ * Abstract class for Google Earth requests. Extend this class to create another
+ * handler for separate GE layer.
+ * 
  * @author Ondrej Vanek
- *
+ * 
  */
-public abstract class GoogleEarthHandler implements HttpHandler {
+public abstract class GoogleEarthHandler implements HttpHandler
+{
 
     /**
      * For identification and logging purposes.
+     * 
      * @return
      */
     protected abstract String getName();
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public void handle(HttpExchange exchange) throws IOException
+    {
 
         long time = System.currentTimeMillis();
+        try
+        {
+            doResponse(exchange);
+        } catch (IOException ioe)
+        {}
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
-        doResponse(exchange);
-
-        System.out.println(getName() + "> request served in "
-                + (System.currentTimeMillis() - time) + "ms.");
+        System.out.println(getName() + "> request served in " + (System.currentTimeMillis() - time)
+                + "ms.");
     }
 
     /**
      * Create Response in form of a KML file and send it.
+     * 
      * @param exchange
      * @throws IOException
      */
-    protected void doResponse(HttpExchange exchange) throws IOException {
+    protected void doResponse(HttpExchange exchange) throws IOException
+    {
         Headers responseHeaders = exchange.getResponseHeaders();
-        responseHeaders.add("Content-Type",
-        "application/vnd.google-earth.kml+xml; charset=UTF-8");
+        responseHeaders.add("Content-Type", "application/vnd.google-earth.kml+xml; charset=UTF-8");
         responseHeaders.add("Content-Encoding", "identity");
         String content = "";
         content = createContentFromEnvironment(exchange.getRequestURI().getQuery());
 
-        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+        if(System.getProperty("os.name").toLowerCase().contains("win"))
+        {
             exchange.sendResponseHeaders(200, content.length()); // content.length());
-        } else {
+        } else
+        {
             exchange.sendResponseHeaders(200, 0); // content.length());
         }
         OutputStream output = exchange.getResponseBody();
@@ -59,12 +72,13 @@ public abstract class GoogleEarthHandler implements HttpHandler {
     }
 
     /**
-     * This method should be implemented to provide the KML file in form of a String.
-     * Use {@link KmlFileCreator} for KML creation, then use {@link KmlFileCreator#getKML()}
-     * to retrieve the KML String and return this String.
+     * This method should be implemented to provide the KML file in form of a
+     * String. Use {@link KmlFileCreator} for KML creation, then use
+     * {@link KmlFileCreator#getKML()} to retrieve the KML String and return
+     * this String.
+     * 
      * @return
      */
     protected abstract String createContentFromEnvironment(String query);
 
 }
-
