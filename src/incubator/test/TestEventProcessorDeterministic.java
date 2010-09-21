fed9db410a44e1362d95dd2d10d6305114808382
Antonin Komenda <997305+gree7@users.noreply.github.com>
2010-09-21 12:55:37 +0200

    Added event processor deterministic test (incubator)

---
+++ src/incubator/test/TestEventProcessorDeterministic.java
@@ -0,0 +1,33 @@
+package incubator.test;
+
+import cz.agents.alite.common.event.Event;
+import cz.agents.alite.common.event.EventHandler;
+import cz.agents.alite.common.event.EventProcessor;
+
+// TODO: use JUnit
+public class TestEventProcessorDeterministic {
+
+    public static void main(String[] args) {
+        final EventProcessor eventProcessor = new EventProcessor();
+    
+        for (int i = 10; i < 1000; i++) {
+            eventProcessor.addEvent(null, new EventHandler() {
+                
+                @Override
+                public void handleEvent(Event event) {
+                    System.out.println(event);
+                }
+                
+                @Override
+                public EventProcessor getEventProcessor() {
+                    return eventProcessor;
+                }
+                
+            }, null, i, i/10);
+        }
+        
+        eventProcessor.run();
+        
+    }
+
+}
