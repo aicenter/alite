b1a55c43860c8745b7ecd2322f82029031bdfd88
Milenovsky <Milenovsky@localhost>
2011-03-09 21:49:06 +0100

    new vis: Points refactored

--- src/incubator/visprotocol/vis/element/Points.java
+++ src/incubator/visprotocol/vis/element/Points.java
@@ -7,41 +7,44 @@ import java.util.LinkedList;
 import cz.agents.alite.vis.element.Point;
 import cz.agents.alite.vis.element.aggregation.PointElements;
 
-// TODO: do not extend LinkedList<Point>, but composite them (create new field points)
 // TODO: implementing the old PointElements interface is not probably good idea, try to remove it
-public class Points extends LinkedList<Point> implements Serializable, PointElements {
+public class Points implements Serializable, PointElements {
 
     private static final long serialVersionUID = -1704273440826602143L;
 
     private final Color color;
     private final int strokeWidth;
 
+    private final LinkedList<Point> points;
+
     public Points(Color color, int strokeWidth) {
-        this.color = color;
-        this.strokeWidth = strokeWidth;
+	points = new LinkedList<Point>();
+	this.color = color;
+	this.strokeWidth = strokeWidth;
     }
 
     public Points(PointElements pointElements) {
-        for (Point point : pointElements.getPoints()) {
-            add(point);
-        }
-        color = pointElements.getColor();
-        strokeWidth = pointElements.getStrokeWidth();
+	points = new LinkedList<Point>();
+	for (Point point : pointElements.getPoints()) {
+	    points.add(point);
+	}
+	color = pointElements.getColor();
+	strokeWidth = pointElements.getStrokeWidth();
     }
 
     @Override
     public Color getColor() {
-        return color;
+	return color;
     }
 
     @Override
     public int getStrokeWidth() {
-        return strokeWidth;
+	return strokeWidth;
     }
 
     @Override
     public Iterable<? extends Point> getPoints() {
-        return this;
+	return points;
     }
 
 }
--- src/incubator/visprotocol/vis/sampler/MaxFPSRealTimeSampler.java
+++ src/incubator/visprotocol/vis/sampler/MaxFPSRealTimeSampler.java
@@ -35,5 +35,4 @@ public abstract class MaxFPSRealTimeSampler implements Sampler {
     }
 
     protected abstract void sample();
-
 }
