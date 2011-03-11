54e54913e1d27f1df3625564f974d2e359373506
Antonin Komenda <997305+gree7@users.noreply.github.com>
2011-03-11 11:16:32 +0100

    Removed PointElements from Points class

--- src/incubator/visprotocol/vis/element/Points.java
+++ src/incubator/visprotocol/vis/element/Points.java
@@ -7,44 +7,21 @@ import java.util.LinkedList;
 import cz.agents.alite.vis.element.Point;
 import cz.agents.alite.vis.element.aggregation.PointElements;
 
-// TODO: implementing the old PointElements interface is not probably good idea, try to remove it
-public class Points implements Serializable, PointElements {
+public class Points implements Serializable {
 
     private static final long serialVersionUID = -1704273440826602143L;
 
-    private final Color color;
-    private final int strokeWidth;
-
-    private final LinkedList<Point> points;
-
-    public Points(Color color, int strokeWidth) {
-	points = new LinkedList<Point>();
-	this.color = color;
-	this.strokeWidth = strokeWidth;
-    }
+    public final LinkedList<Point> points;
+    public final Color color;
+    public final int strokeWidth;
 
     public Points(PointElements pointElements) {
-	points = new LinkedList<Point>();
-	for (Point point : pointElements.getPoints()) {
-	    points.add(point);
-	}
-	color = pointElements.getColor();
-	strokeWidth = pointElements.getStrokeWidth();
-    }
-
-    @Override
-    public Color getColor() {
-	return color;
-    }
-
-    @Override
-    public int getStrokeWidth() {
-	return strokeWidth;
-    }
-
-    @Override
-    public Iterable<? extends Point> getPoints() {
-	return points;
+        points = new LinkedList<Point>();
+        for (Point point : pointElements.getPoints()) {
+            points.add(point);
+        }
+        color = pointElements.getColor();
+        strokeWidth = pointElements.getStrokeWidth();
     }
 
 }
