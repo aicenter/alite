c67eb54231cb83dc8d1082051bb210451e83fee7
Milenovsky <Milenovsky@localhost>
2011-04-06 17:34:21 +0200

    Removed commented code

--- src/incubator/visprotocol/vis/layer/common/SimInfoLayer.java
+++ src/incubator/visprotocol/vis/layer/common/SimInfoLayer.java
@@ -17,18 +17,6 @@ public class SimInfoLayer extends AbstractLayer {
         this.holder = holder;
     }
 
-
-    // @Override
-    // public Structure pull() {
-    // Structure struct = new Structure(CommonKeys.STRUCT_PART, env.getTime());
-    // if (hasType("Name")) {
-    // Element e = struct.getRoot("World").getFolder("Other").getElement("Info", "Name");
-    // setParameter(e, "Name", "Undead land");
-    // }
-    // struct.setType(CommonKeys.STRUCT_PART);
-    // return struct;
-    // }
-
     @Override
     protected void generateFrame() {
         setTime(holder.getCurrentTimeMillis());
