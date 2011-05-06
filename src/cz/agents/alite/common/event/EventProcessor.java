fae947fdcb1f9c84f248a51c55667c764e02aea8
Milenovsky <Milenovsky@localhost>
2011-05-06 16:09:33 +0200

    Added some methods

--- src/cz/agents/alite/common/event/EventProcessor.java
+++ src/cz/agents/alite/common/event/EventProcessor.java
@@ -9,24 +9,21 @@ import java.util.logging.Logger;
 
 /**
  * The event processor is a wrapper for an event-queue cycle.
- *
- * On the fly, the processor lines up events added into it, and "sends" them
- * to their particular recipients in a particular order. By sending,
- * it is meant to call a {@link EventHandler}.handleEvent() method, with the
- * event as an argument. An event handler needs to be registered into the
- * processor to receive the events.
- *
- * The event is sent to all event handlers registered with the EventProcessor,
- * if its recipient is <code>null</code> and it is sent to the particular event
- * handler otherwise.
- *
- * The event-queue cycle can be paused/un-paused by the setRunning() method.
- * By the default, the cycle is started running.
- *
- * The time of currently processed events can be obtained by the getCurrentTime()
- * method.
- *
- *
+ * 
+ * On the fly, the processor lines up events added into it, and "sends" them to their particular
+ * recipients in a particular order. By sending, it is meant to call a {@link EventHandler}
+ * .handleEvent() method, with the event as an argument. An event handler needs to be registered
+ * into the processor to receive the events.
+ * 
+ * The event is sent to all event handlers registered with the EventProcessor, if its recipient is
+ * <code>null</code> and it is sent to the particular event handler otherwise.
+ * 
+ * The event-queue cycle can be paused/un-paused by the setRunning() method. By the default, the
+ * cycle is started running.
+ * 
+ * The time of currently processed events can be obtained by the getCurrentTime() method.
+ * 
+ * 
  * @author Antonin Komenda
  */
 public class EventProcessor {
@@ -51,7 +48,8 @@ public class EventProcessor {
                     try {
                         thread.wait();
                     } catch (InterruptedException ex) {
-                        Logger.getLogger(EventProcessor.class.getName()).log(Level.SEVERE, null, ex);
+                        Logger.getLogger(EventProcessor.class.getName())
+                                .log(Level.SEVERE, null, ex);
                     }
                 }
             }
@@ -64,21 +62,33 @@ public class EventProcessor {
         running = false;
     }
 
+    /** ends the simulation */
+    public void clearQueue() {
+        eventQueue.clear();
+    }
+
     public void addEvent(EventType type, EventHandler recipient, String owner, Object content) {
         addEvent(type, recipient, owner, content, 1);
     }
 
     /**
      * Add an event into the queue of the event processor.
-     *
-     *
-     * @param type the type of the event (see {@link EventType})
-     * @param recipient the target of the event (use <code>null</code> for all registered event handlers)
-     * @param owner a "deprecated" version of the recipient
-     * @param content the payload of the event (by the user specified data)
-     * @param deltaTime the duration (in milliseconds) from now till the time when the event should take place (be send to its recipients)
+     * 
+     * 
+     * @param type
+     *            the type of the event (see {@link EventType})
+     * @param recipient
+     *            the target of the event (use <code>null</code> for all registered event handlers)
+     * @param owner
+     *            a "deprecated" version of the recipient
+     * @param content
+     *            the payload of the event (by the user specified data)
+     * @param deltaTime
+     *            the duration (in milliseconds) from now till the time when the event should take
+     *            place (be send to its recipients)
      */
-    public void addEvent(EventType type, EventHandler recipient, String owner, Object content, long deltaTime) {
+    public void addEvent(EventType type, EventHandler recipient, String owner, Object content,
+            long deltaTime) {
         // TODO: refactorize the recipients/owners/senders/groups and similar
         if (deltaTime < 1) {
             throw new IllegalArgumentException("deltaTime must be greater then zero!");
@@ -137,4 +147,3 @@ public class EventProcessor {
     }
 
 }
-
