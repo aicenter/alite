97c43f6880e157bc6607d928b350fdec6d5a21ce
Antonin Komenda <antonin.komenda@agents.felk.cvut.cz>
2011-08-12 17:51:48 +0200

    Added direct messaging test

--- src/main/java/cz/agents/alite/communication/protocol/Protocol.java
+++ src/main/java/cz/agents/alite/communication/protocol/Protocol.java
@@ -6,22 +6,6 @@ package cz.agents.alite.communication.protocol;
  */
 public interface Protocol {
 
-    //TODO: temporary constant - remove
-//    public static final Protocol TEST = new Protocol() {
-//
-//        @Override
-//        public String getName() {
-//            return "TEST";
-//        }
-//
-//        @Override
-//        public boolean equals(Protocol protocol) {
-//            return getName().equals(protocol.getName());
-//        }
-//    };
-
-    //TODO: add state controll mechanisms
-
     /**
      * Returns name of the protocol. It should be buid as PROTOCOL_PREFFIX + protocol instance name
      *
@@ -31,7 +15,7 @@ public interface Protocol {
 
 
     /**
-     * Comparator for protocols. 
+     * Comparator for protocols.
      *
      * @param protocol
      * @return true if protocols are equal
---
+++ src/test/java/cz/agents/alite/communication/TestMessaging.java
@@ -0,0 +1,173 @@
+package cz.agents.alite.communication;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import cz.agents.alite.communication.channel.CommunicationChannelException;
+import cz.agents.alite.communication.channel.DirectCommunicationChannel;
+import cz.agents.alite.communication.channel.UnknownReceiversException;
+import cz.agents.alite.communication.content.error.ErrorContent;
+import cz.agents.alite.communication.content.error.ErrorMessageHandler;
+import cz.agents.alite.communication.protocol.Performative;
+import cz.agents.alite.communication.protocol.Protocol;
+import cz.agents.alite.communication.protocol.ProtocolContent;
+import cz.agents.alite.communication.protocol.ProtocolMessageHandler;
+
+public class TestMessaging {
+
+    private static final String data = "DATA";
+    private static final String addressReceiver = "addressReceiver";
+    private static final Collection<String> addressReceiversUnknown = new LinkedList<String>(Arrays.asList(new String[] {"addressDummy1", "addressDummy2"}));
+    private static final String addressSender = "addressSender";
+    private static final Protocol protocol = new Protocol() {
+
+        @Override
+        public String getName() {
+            return "TEST";
+        }
+
+        @Override
+        public boolean equals(Protocol protocol) {
+            return getName().equals(protocol.getName());
+        }
+    };
+
+    private static final Performative performative = Performative.DONE;
+
+    private static MessageWrapper received;
+    private static MessageWrapper errorReceived;
+    private static DefaultCommunicator communicatorSender;
+    private static DefaultCommunicator communicatorReceiver;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws CommunicationChannelException {
+        received = new MessageWrapper();
+        errorReceived = new MessageWrapper();
+
+        communicatorSender = new DefaultCommunicator(addressSender);
+        communicatorReceiver = new DefaultCommunicator(addressReceiver);
+
+        // create communication channels
+        communicatorSender.addChannel(new DirectCommunicationChannel(communicatorSender));
+        communicatorReceiver.addChannel(new DirectCommunicationChannel(communicatorReceiver));
+    }
+
+    @Before
+    public void setUp() throws CommunicationChannelException {
+        received.message = null;
+        errorReceived.message = null;
+    }
+
+    @Test
+    public void directSend() {
+
+        // create message handlers
+        ErrorMessageHandler errorMessageHandler = new ErrorMessageHandler() {
+
+            @Override
+            public void handleMessage(Message message, ErrorContent content) {
+                Assert.assertTrue(false);
+            }
+
+        };
+        communicatorSender.addMessageHandler(errorMessageHandler);
+
+        ProtocolMessageHandler protocolMessageHandler = new ProtocolMessageHandler(protocol, performative, "") {
+
+            @Override
+            public void handleMessage(Message message, ProtocolContent content) {
+                Assert.assertEquals(data, content.getData());
+
+                received.message = message;
+            }
+
+        };
+        communicatorReceiver.addMessageHandler(protocolMessageHandler);
+
+        // send message
+        Message message = communicatorSender.createMessage(new ProtocolContent(protocol, performative, data,""));
+        message.addReceiver(addressReceiver);
+        communicatorSender.sendMessage(message);
+
+
+        // test
+        Assert.assertNotNull(received.message);
+        Assert.assertEquals(data, received.message.getContent().getData());
+        Assert.assertEquals(message.getId(), received.message.getId());
+        Assert.assertEquals(message.getSender(), addressSender);
+        Assert.assertTrue(message.getReceivers().contains(addressReceiver));
+
+        // cleanup
+        communicatorSender.removeMessageHandler(errorMessageHandler);
+        communicatorReceiver.removeMessageHandler(protocolMessageHandler);
+    }
+
+    @Test
+    public void directSendUnknownReceiver() {
+
+        // create message handlers
+        ErrorMessageHandler errorMessageHandler = new ErrorMessageHandler() {
+
+            @Override
+            public void handleMessage(Message message, ErrorContent content) {
+                Assert.assertTrue((((UnknownReceiversException) content.getData()).getUnknownReceivers().containsAll(addressReceiversUnknown)));
+                Assert.assertFalse((((UnknownReceiversException) content.getData()).getUnknownReceivers().contains(addressReceiver)));
+
+                errorReceived.message = message;
+            }
+
+        };
+        communicatorSender.addMessageHandler(errorMessageHandler);
+
+        ProtocolMessageHandler protocolMessageHandler = new ProtocolMessageHandler(protocol, performative,"") {
+
+            @Override
+            public void handleMessage(Message message, ProtocolContent content) {
+                Assert.assertEquals(data, content.getData());
+
+                received.message = message;
+            }
+
+        };
+        communicatorReceiver.addMessageHandler(protocolMessageHandler);
+
+        // send message
+        Message message = communicatorSender.createMessage(new ProtocolContent(protocol, performative, data,""));
+        message.addReceivers(addressReceiversUnknown);
+        message.addReceiver(addressReceiver);
+        communicatorSender.sendMessage(message);
+
+
+        // test - delivered
+        Assert.assertNotNull(received.message);
+        Assert.assertEquals(data, received.message.getContent().getData());
+        Assert.assertEquals(message.getId(), received.message.getId());
+        Assert.assertEquals(message.getSender(), addressSender);
+        Assert.assertTrue(message.getReceivers().contains(addressReceiver));
+
+        // test - undelivered
+        Assert.assertNotNull(errorReceived.message);
+        Assert.assertTrue(((UnknownReceiversException) errorReceived.message.getContent().getData()).getUnknownReceivers().containsAll(addressReceiversUnknown));
+        Assert.assertFalse((((UnknownReceiversException) errorReceived.message.getContent().getData()).getUnknownReceivers().contains(addressReceiver)));
+        Assert.assertEquals(errorReceived.message.getSender(), addressSender);
+        Assert.assertTrue(errorReceived.message.getReceivers().contains(addressSender));
+
+        // cleanup
+        communicatorSender.removeMessageHandler(errorMessageHandler);
+        communicatorReceiver.removeMessageHandler(protocolMessageHandler);
+    }
+
+    private static class MessageWrapper {
+
+        public Message message = null;
+
+    }
+
+}
