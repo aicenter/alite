9092a2857744f92fab95408018af9021e1c0e1c8
Michal Stolba <12560758+stolba@users.noreply.github.com>
2012-08-31 11:47:38 +0200

    naive implementation of cylinder zone intersections - TODO: better implementation

--- src/main/java/cz/agents/alite/planner/spatialmaneuver/zone/CylinderZone.java
+++ src/main/java/cz/agents/alite/planner/spatialmaneuver/zone/CylinderZone.java
@@ -1,5 +1,6 @@
 package cz.agents.alite.planner.spatialmaneuver.zone;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.vecmath.Point3d;
@@ -67,7 +68,28 @@ public class CylinderZone implements Zone {
 
 	@Override
 	public List<Point3d> findLineIntersections(Point3d point1, Point3d point2) {
-		throw new RuntimeException("Not implemented yet!");
+		final double ret1 = testLineForFirstPoint(point1.x, point1.y, point1.z, point2.x, point2.y,point2.z);
+		final double ret2 = testLineForFirstPoint(point2.x, point2.y,point2.z, point1.x, point1.y, point1.z);
+		
+		List<Point3d> out = new LinkedList<Point3d>();
+		
+		double dx = point2.x - point1.x;
+        double dy = point2.y - point1.y;
+        double dz = point2.z - point1.z;
+        double outx = point1.x + dx * ret1;
+        double outy = point1.y + dy * ret1;
+        double outz = point1.z + dz * ret1;
+        out.add(new Point3d(outx, outy, outz));
+        
+        dx = point1.x - point2.x;
+        dy = point1.y - point2.y;
+        dz = point1.z - point2.z;
+        outx = point2.x + dx * ret2;
+        outy = point2.y + dy * ret2;
+        outz = point2.z + dz * ret2;
+        out.add(new Point3d(outx, outy, outz));
+        
+        return out;
 	}
 
     private double testLineForFirstPoint(final double point1x, final double point1y,
