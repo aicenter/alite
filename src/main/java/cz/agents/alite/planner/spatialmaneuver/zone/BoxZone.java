364de5e318dacd9aaeacea6a2f58ad7f6b846ce4
Antonin Komenda <997305+gree7@users.noreply.github.com>
2012-10-30 17:17:44 +0100

    Fixed implementation of new methods in Zone interface

--- src/main/java/cz/agents/alite/planner/spatialmaneuver/zone/BoxZone.java
+++ src/main/java/cz/agents/alite/planner/spatialmaneuver/zone/BoxZone.java
@@ -27,7 +27,17 @@ public class BoxZone implements Zone {
         if (outPoint == null) {
             outPoint = new Point3d();
         }
-        return CheckLineBox(new Point3d(0, 0, 0), new Point3d(axes), point1, point2, outPoint);
+        return checkLineBox(new Point3d(0, 0, 0), new Point3d(axes), point1, point2, outPoint);
+    }
+
+    @Override
+    public boolean testLine(Point3d point1, Point3d point2) {
+        return testLine(point1, point2, null);
+    }
+
+    @Override
+    public List<Point3d> findLineIntersections(Point3d point1, Point3d point2) {
+        throw new RuntimeException("Not implemented yet!");
     }
 
     @Override
@@ -35,7 +45,7 @@ public class BoxZone implements Zone {
         visitor.visit(this);
     }
 
-    boolean GetIntersection(double fDst1, double fDst2, Point3d P1, Point3d P2, Point3d hit) {
+    private boolean getIntersection(double fDst1, double fDst2, Point3d P1, Point3d P2, Point3d hit) {
         if ((fDst1 * fDst2) >= 0.0)
             return false;
         if (fDst1 == fDst2)
@@ -48,7 +58,7 @@ public class BoxZone implements Zone {
         return true;
     }
 
-    boolean InBox(Point3d Hit, Point3d B1, Point3d B2, final int Axis) {
+    private boolean isInBox(Point3d Hit, Point3d B1, Point3d B2, final int Axis) {
         if (Axis == 1 && Hit.z > B1.z && Hit.z < B2.z && Hit.y > B1.y && Hit.y < B2.y)
             return true;
         if (Axis == 2 && Hit.z > B1.z && Hit.z < B2.z && Hit.x > B1.x && Hit.x < B2.x)
@@ -60,7 +70,7 @@ public class BoxZone implements Zone {
 
     // returns true if line (L1, L2) intersects with the box (B1, B2)
     // returns intersection point in Hit
-    boolean CheckLineBox(Point3d B1, Point3d B2, Point3d L1, Point3d L2, Point3d Hit) {
+    private boolean checkLineBox(Point3d B1, Point3d B2, Point3d L1, Point3d L2, Point3d Hit) {
         if (L2.x < B1.x && L1.x < B1.x)
             return false;
         if (L2.x > B2.x && L1.x > B2.x)
@@ -77,24 +87,15 @@ public class BoxZone implements Zone {
             Hit.set(L1);
             return true;
         }
-        if ((GetIntersection(L1.x - B1.x, L2.x - B1.x, L1, L2, Hit) && InBox(Hit, B1, B2, 1))
-                || (GetIntersection(L1.y - B1.y, L2.y - B1.y, L1, L2, Hit) && InBox(Hit, B1, B2, 2))
-                || (GetIntersection(L1.z - B1.z, L2.z - B1.z, L1, L2, Hit) && InBox(Hit, B1, B2, 3))
-                || (GetIntersection(L1.x - B2.x, L2.x - B2.x, L1, L2, Hit) && InBox(Hit, B1, B2, 1))
-                || (GetIntersection(L1.y - B2.y, L2.y - B2.y, L1, L2, Hit) && InBox(Hit, B1, B2, 2))
-                || (GetIntersection(L1.z - B2.z, L2.z - B2.z, L1, L2, Hit) && InBox(Hit, B1, B2, 3)))
+        if ((getIntersection(L1.x - B1.x, L2.x - B1.x, L1, L2, Hit) && isInBox(Hit, B1, B2, 1))
+                || (getIntersection(L1.y - B1.y, L2.y - B1.y, L1, L2, Hit) && isInBox(Hit, B1, B2, 2))
+                || (getIntersection(L1.z - B1.z, L2.z - B1.z, L1, L2, Hit) && isInBox(Hit, B1, B2, 3))
+                || (getIntersection(L1.x - B2.x, L2.x - B2.x, L1, L2, Hit) && isInBox(Hit, B1, B2, 1))
+                || (getIntersection(L1.y - B2.y, L2.y - B2.y, L1, L2, Hit) && isInBox(Hit, B1, B2, 2))
+                || (getIntersection(L1.z - B2.z, L2.z - B2.z, L1, L2, Hit) && isInBox(Hit, B1, B2, 3)))
             return true;
 
         return false;
     }
 
-	@Override
-	public boolean testLine(Point3d point1, Point3d point2) {
-		throw new RuntimeException("Not implemented yet!");
-	}
-
-	@Override
-	public List<Point3d> findLineIntersections(Point3d point1, Point3d point2) {
-		throw new RuntimeException("Not implemented yet!");
-	}
 }
