90f7de3065ffdf05866d2da9623ee6833b2ad52a
stolba <12560758+stolba@users.noreply.github.com>
2014-01-30 11:37:21 +0100

    performer javadoc

--- src/main/java/cz/agents/alite/communication/channel/CommunicationPerformerChannel.java
+++ src/main/java/cz/agents/alite/communication/channel/CommunicationPerformerChannel.java
@@ -1,8 +1,20 @@
 package cz.agents.alite.communication.channel;
 
-
+/**
+ * Used to implement passive communication - in order to receive any messages 
+ * via a MessageHandler, the performReceive() method must be invoked. 
+ * The message (if any) is then delivered using the same thread which called 
+ * the performReceive() method. The call is non-blocking.
+ * 
+ * @author stolba
+ *
+ */
 public interface CommunicationPerformerChannel extends CommunicationChannel {
 	
+	/**
+	 * Use the current thread to poll a message and give it to the handlers.
+	 * The call is non-blocking.
+	 */
 	public void performReceive();
 	
 }
